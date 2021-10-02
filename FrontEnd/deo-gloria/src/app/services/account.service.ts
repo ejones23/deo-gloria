7702334c9ee72679419f714b84fcc2bb1da4e837
Emmanuel Jones <jones.e18@gmail.com>
2021-10-02 13:53:26 -0600

    Wire SignUp component to signup endpoint

--- FrontEnd/deo-gloria/src/app/services/account.service.ts
+++ FrontEnd/deo-gloria/src/app/services/account.service.ts
@@ -10,12 +10,19 @@ const httpOptions = {
   })
 };
 const loginUrl: string = 'http://localhost:3000/api/login';
+const signupUrl: string = 'http://localhost:3000/api/signup';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
   constructor(private http: HttpClient) { }
+
+  signUp(username: string, password: string) {
+    let credentials = { 'username': username, 'password': password };
+    this.http.post<any>(signupUrl, credentials, httpOptions)
+      .subscribe(res => console.log(res));
+  }
 
   logIn(username: string, password: string) {
     let credentials = { 'username': username, 'password': password };
--- FrontEnd/deo-gloria/src/app/signup/signup.component.ts
+++ FrontEnd/deo-gloria/src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../services/account.service';
 
 @Component({
   selector: 'app-signup',
@@ -11,12 +12,12 @@ export class SignupComponent implements OnInit {
   password: string = '';
   confirmPassword: string = '';
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
   onSubmit() {
-    
+    this.accountService.signUp(this.username, this.password);
   }
 }
